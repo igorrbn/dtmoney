bf446257f4c6e42f9ee2c64d785e961d3d87156e
Igor <igorr.snoopy@hotmail.com>
2022-01-07 14:54:31 -0300

    Att2
    Att2

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -4,6 +4,7 @@ export const GlobalStyle = createGlobalStyle`
   :root {
     --background: #f0F2f5; 
     --red: #E52E40;
+    --green: #33cc95;
     --blue: #5429CC;
 
     --blue-light: #6933FF;
@@ -29,11 +30,21 @@ export const GlobalStyle = createGlobalStyle`
    @media (min-width: 720px) {
       font-size: 87.5%; 
    } 
- }
- body {
+  }
+
+  body {
     background: var(--background);
     -webkit-font-smoothing: antialiased;
    }  
+
+   body, input, textarea, button {
+     font-family: 'Poppins', sans-serif;
+     font-weight: 400;
+   } 
+
+   h1, h3, h4, h5, h6, strong {
+     font-weight: 600;
+   }
 
    button {
    cursor: pointer;
