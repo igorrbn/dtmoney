e073df6f58d83e648c334dbae39a0e828334a2a0
Igor <igorr.snoopy@hotmail.com>
2022-01-21 19:47:01 -0300

    Att
    Att

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,16 @@ import Modal from 'react-modal';
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { NewTransactionModal } from "./components/NewTransactionModal";
+import { TransactionsProvider } from './TransactionsContext';
 
 import { GlobalStyle } from "./styles/global";
+
 
 Modal.setAppElement('#root');
 
 export function App() {
   const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
-
+  
   function handleOpenNewTransactionModal(){
     setIsNewTransactionModalOpen(true);
   }
@@ -20,7 +22,7 @@ export function App() {
   }
 
   return (
-    <>
+    <TransactionsProvider>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
 
       <Dashboard />   
@@ -31,6 +33,6 @@ export function App() {
       />   
 
       <GlobalStyle />
-    </>
+    </TransactionsProvider>
   );
 }
